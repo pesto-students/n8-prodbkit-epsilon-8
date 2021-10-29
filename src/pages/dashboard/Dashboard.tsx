494454f9b84312748c8b975cb5dd099809eea671
Abhishek khanna <ABHISHEKKHANNA1995@GMAIL.COM>
2021-10-29 22:39:23 +0530

    [Dashboard] feat: add dashboard view (#11)
    * feat: add audit logs view
    
    * feat: add Dashboard View
    
    * fix: amend changes post API integration

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,18 +1,166 @@
+import { Card, Col, Row } from 'antd';
+import axios from 'axios';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useQuery } from 'react-query';
+import Header from 'shared/components/atoms/header/Header';
+import { getURL } from 'shared/utils/api';
+import { VictoryBar, VictoryChart, VictoryLine, VictoryPolarAxis, VictoryTheme } from 'victory';
+
+import {
+  IDashboardStatsDatabaseData,
+  IDashboardStatsOnboardedTeamData,
+  IDashboardStatsTeamData,
+} from './dashboard.interface';
+import styles from './dashboard.module.scss';
 
 const PAGE_TITLE = 'Dashboard';
 
-const Dashboard: React.FC = () => {
+const fetchDashboardStats = () => {
+  return axios.get(getURL('/auth/dashboard-stats'), {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt_token')}`,
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const Teams: React.FC = () => {
+  const dashboardStatsAPIResponse = useQuery('dashboard-stats', fetchDashboardStats);
+  const { data } = dashboardStatsAPIResponse;
+
+  function transformDataForBarChart(teamStats: IDashboardStatsTeamData[] = []) {
+    return teamStats.map(
+      ({ name, members }: IDashboardStatsTeamData): { team: string; members: number } => ({
+        team: name,
+        members,
+      }),
+    );
+  }
+
+  function transformDataForPolarChart(databaseStats: IDashboardStatsDatabaseData[] = []) {
+    return databaseStats.map(
+      ({ name, teams }: IDashboardStatsDatabaseData): { x: string; y: number } => ({
+        x: name,
+        y: teams,
+      }),
+    );
+  }
+
+  function transformDataForLineChart(onboardedTeamStats: IDashboardStatsOnboardedTeamData[] = []) {
+    return onboardedTeamStats.map(
+      ({ month, teamCount }: IDashboardStatsOnboardedTeamData): { x: string; y: number } => ({
+        x: month,
+        y: teamCount,
+      }),
+    );
+  }
+
   return (
-    <>
+    <div className={styles.dashboardWrapper}>
       <Helmet>
         <meta name="Pro-DB Kit" content="Ninja 8 demo app" charSet="utf-8" />
         <title>{PAGE_TITLE}</title>
       </Helmet>
-      <h3>{PAGE_TITLE}</h3>
-    </>
+      <Header
+        title={PAGE_TITLE}
+        buttonText=""
+        buttonCallback={() => {
+          console.log('Dashboard');
+        }}
+      />
+
+      <Row gutter={16}>
+        <Col span={9} offset={2}>
+          <Card
+            title="Teams and database count"
+            bordered={false}
+            loading={dashboardStatsAPIResponse.isLoading}
+          >
+            <VictoryChart
+              polar
+              theme={VictoryTheme.material}
+              startAngle={90}
+              endAngle={450}
+              padding={60}
+            >
+              <VictoryPolarAxis
+                tickValues={[0, 45, 90, 135, 180, 225, 270, 315]}
+                labelPlacement="vertical"
+              />
+              <VictoryBar
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+                style={{ data: { fill: styles.primaryColor, width: 40 } }}
+                data={
+                  data?.data?.stats?.database.length
+                    ? transformDataForPolarChart(data.data.stats.database)
+                    : []
+                }
+              />
+            </VictoryChart>
+          </Card>
+        </Col>
+        <Col span={9} offset={2}>
+          <Card
+            title="Teams and member count"
+            bordered={false}
+            loading={dashboardStatsAPIResponse.isLoading}
+          >
+            <VictoryChart domainPadding={20}>
+              <VictoryBar
+                style={{ data: { fill: styles.primaryColor } }}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+                data={
+                  data?.data?.stats?.teams.length
+                    ? transformDataForBarChart(data.data.stats.teams)
+                    : []
+                }
+                x="team"
+                y="members"
+              />
+            </VictoryChart>
+          </Card>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={9} offset={8}>
+          <Card
+            title="Teams onboarded per month"
+            bordered={false}
+            loading={dashboardStatsAPIResponse.isLoading}
+          >
+            <VictoryChart>
+              <VictoryChart theme={VictoryTheme.material}>
+                <VictoryLine
+                  domain={{ y: [0, 5] }}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 },
+                  }}
+                  style={{
+                    data: { stroke: '#26519e' },
+                    parent: { border: '1px solid #ccc' },
+                  }}
+                  labels={({ datum }) => datum.y}
+                  data={
+                    data?.data?.stats?.onboardedTeams.length
+                      ? transformDataForLineChart(data.data.stats.onboardedTeams)
+                      : []
+                  }
+                />
+              </VictoryChart>
+            </VictoryChart>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
-export default Dashboard;
+export default Teams;
---
+++ src/pages/dashboard/dashboard.interface.ts
@@ -0,0 +1,14 @@
+export interface IDashboardStatsTeamData {
+  name: string;
+  members: number;
+}
+
+export interface IDashboardStatsDatabaseData {
+  name: string;
+  teams: number;
+}
+
+export interface IDashboardStatsOnboardedTeamData {
+  month: string;
+  teamCount: number;
+}
