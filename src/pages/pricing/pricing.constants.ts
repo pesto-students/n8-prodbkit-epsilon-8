70f2b59781ce035140a7d582bbaedb18979f245c
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2021-10-31 11:12:04 +0530

    feat: update static content for landing and pricing page

--- src/pages/pricing/pricing.constants.ts
+++ src/pages/pricing/pricing.constants.ts
@@ -1,48 +1,48 @@
 import { IPricingData, IPricingDataObject } from './pricing.interface';
 
 export const pricingDataObject: IPricingDataObject = {
-  title: 'Deliver the apps your team needs now',
+  title: 'Deliver the experiences that your team needs now',
   subtitle: 'Pro DB kit scales for any organization — from startups to Fortune 500s.',
 };
 
 export const pricingData: IPricingData[] = [
   {
-    type: 'Free',
+    type: 'Forever Free',
     pricePerMonth: '0',
     buttonText: 'Get started',
-    summary: 'For developers building apps, with no creator restrictions',
+    summary: 'For small teams of people 1-5',
     listTitle: 'Features',
     features: [
-      'Build unlimited apps (creator view only)',
-      'Build apps on top of any database or API',
-      'Write, share, and reuse queries with Query Library',
-      'Reuse up to 5 modules (groups of components) across apps',
+      'Create upto 3 teams',
+      'Manage upto 3 databases',
+      'Add upto 3 members / team',
+      'Create upto 30 database credentials for your users',
     ],
   },
   {
-    type: 'Team',
+    type: 'Premium',
     pricePerMonth: '1000',
     buttonText: 'Try for free',
-    summary: 'For developers building apps, with no creator restrictions',
+    summary: 'For business',
     listTitle: 'Features',
     features: [
-      'Build unlimited apps (creator view only)',
-      'Build apps on top of any database or API',
-      'Write, share, and reuse queries with Query Library',
-      'Reuse up to 5 modules (groups of components) across apps',
+      'Create upto 10 teams',
+      'Manage upto 20 databases',
+      'Add upto 10 members / team',
+      'Create upto 200 database credentials for your users',
     ],
   },
   {
-    type: 'Business',
+    type: 'Enterprise',
     pricePerMonth: '3000',
     buttonText: 'Try for free',
-    summary: 'For developers building apps, with no creator restrictions',
+    summary: 'For businesses who have dedicated infrastructure teams',
     listTitle: 'Features',
     features: [
-      'Build unlimited apps (creator view only)',
-      'Build apps on top of any database or API',
-      'Write, share, and reuse queries with Query Library',
-      'Reuse up to 5 modules (groups of components) across apps',
+      'Create upto 30 teams',
+      'Manage upto 100 databases',
+      'Add upto 30 members / team',
+      'Create upto 2000 database credentials for your users',
     ],
   },
 ];
